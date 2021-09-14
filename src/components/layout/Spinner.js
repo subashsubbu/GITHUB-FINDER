a690d3a8faa6b7f0ab3d621ca7284f4754b5528c
subashsubbu <35239091+subashsubbu@users.noreply.github.com>
2021-09-13 22:12:00 -0500

    added spinner

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -6,7 +6,7 @@ import spinner from './spinner.gif';
 const Spinner = () => {
     return (
         <Fragment>
-            <img src={spinner} alt='loading..' style= {{ width: '200', margin: 'auto', display: 'block'}} />
+            <img src={spinner} alt='loading..' style= {{ width: '10', margin: 'auto', display: 'block'}} />
 
         </Fragment>
     )
